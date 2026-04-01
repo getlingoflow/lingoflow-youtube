321990d5d4e881b3d3f326c6a5508583caa10052
gookabu2077 <gookabu2077@gmail.com>
2026-04-01 13:15:55 +0800

    chore: update project urls to getlingoflow/lingoflow-youtube

--- src/config/url.js
+++ src/config/url.js
@@ -5,10 +5,10 @@ export const URL_CACHE_SUBTITLE = `https://${APP_LCNAME}/subtitle`;
 export const URL_CACHE_DELANG = `https://${APP_LCNAME}/detectlang`;
 export const URL_CACHE_BINGDICT = `https://${APP_LCNAME}/bingdict`;
 
-export const URL_LINGOFLOW_WORKER = "https://github.com/nickyc975/lingoflow-worker";
-export const URL_LINGOFLOW_PROXY = "https://github.com/nickyc975/lingoflow-proxy";
-export const URL_LINGOFLOW_RULES = "https://github.com/nickyc975/lingoflow-rules";
+export const URL_LINGOFLOW_WORKER = "https://github.com/getlingoflow/lingoflow-youtube";
+export const URL_LINGOFLOW_PROXY = "https://github.com/getlingoflow/lingoflow-youtube";
+export const URL_LINGOFLOW_RULES = "https://github.com/getlingoflow/lingoflow-youtube";
 export const URL_LINGOFLOW_RULES_NEW_ISSUE =
-  "https://github.com/nickyc975/lingoflow-rules/issues/new";
+  "https://github.com/getlingoflow/lingoflow-youtube/issues/new";
 export const URL_RAW_PREFIX =
-  "https://raw.githubusercontent.com/nickyc975/lingoflow/master";
+  "https://raw.githubusercontent.com/getlingoflow/lingoflow-youtube/main";
